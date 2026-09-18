9eb5a5f01516a72ec79acdef2532af1672bf3bae
AnastasiaBrykina <anastasiaaa.6934.@gmail.com>
2026-09-18 15:03:22 +0400

    Работа с env

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,6 +1,14 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-svgr/client" />
 
+interface ImportMetaEnv {
+  readonly VITE_CLIENT_KEY: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
+
 declare module '*&as=picture' {
   const picture: {
     img: { src: string; w: number; h: number }
